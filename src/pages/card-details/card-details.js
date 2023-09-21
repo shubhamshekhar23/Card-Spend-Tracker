767f74334e91607d350f37c874566ccfecd3283b
Shubham Shekhar <shubham.ae.iitkgp@gmail.com>
2023-09-21 23:26:21 +0530

    slider added

--- src/pages/card-details/card-details.js
+++ src/pages/card-details/card-details.js
@@ -17,6 +17,7 @@ import { UilShare, UilHistory } from "@iconscout/react-unicons";
 export default function CardDetails() {
   const { cardsData, setCardsData } = useContext(Cards_data);
   let [historyData, setHistoryData] = useState({});
+  let [sliderPosition, setSliderPosition] = useState(0); // can hahve 0,1,2
 
   const [card, setCard] = useState({});
   const router = useRouter();
@@ -45,6 +46,28 @@ export default function CardDetails() {
     if (card.id) fetchData();
   }, [card]);
 
+  function sliderButtonClick() {
+    if (sliderPosition == 2) setSliderPosition(0);
+    else setSliderPosition(sliderPosition + 1);
+  }
+
+  function classForSliderPanel() {
+    let classNames = [styles.slider_panel];
+
+    if (sliderPosition == 1) {
+      classNames.push(styles.slider_panel_level_one);
+    }
+    if (sliderPosition == 2) {
+      classNames.push(styles.slider_panel_level_two);
+    }
+    return classNames.join(" ");
+  }
+
+  function classForTitle() {
+    if (sliderPosition !== 0) return styles.title_complete_slider;
+    return "";
+  }
+
   return (
     <main className={styles.main}>
       <UilArrowLeft
@@ -53,13 +76,22 @@ export default function CardDetails() {
         height={40}
         width={40}
       />
-      <h1>Salary Card</h1>
+      <h1 className={classForTitle()}>Salary Card</h1>
       <BankCard className={styles.bank_card} data={card} />
+
       {/* balance share section */}
       <BalanceShareSection card={card} />
 
       {/* history panel */}
-      <TransactionHistory data={historyData} />
+      <div className={classForSliderPanel()}>
+        <div className={styles.slider_btn}>
+          <button
+            className={styles.nav_btn}
+            onClick={sliderButtonClick}
+          ></button>
+        </div>
+        <TransactionHistory data={historyData} />
+      </div>
     </main>
   );
 }
