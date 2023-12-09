44f352624d707d03a2d426e26dfae0bfb1854a73
Shubham Shekhar <shubham.ae.iitkgp@gmail.com>
2023-12-09 16:10:57 +0530

    plopfile updated and tests added using jest

--- plopfile.js
+++ plopfile.js
@@ -1,5 +1,8 @@
+const path = require("path");
+
 module.exports = function (plop) {
   const currentWorkingDirectory = process.cwd();
+  const name = path.basename(currentWorkingDirectory);
 
   plop.setGenerator("c", {
     description: "Create a new React TypeScript component",
@@ -24,6 +27,27 @@ module.exports = function (plop) {
           currentWorkingDirectory +
           "/{{kebabCase name}}/{{kebabCase name}}.tsx",
         templateFile: "plop-templates/component.tsx.hbs",
+      },
+      {
+        type: "add",
+        path:
+          currentWorkingDirectory +
+          "/{{kebabCase name}}/{{kebabCase name}}.spec.tsx",
+        templateFile: "plop-templates/component.spec.tsx.hbs",
+      },
+    ],
+  });
+
+  plop.setGenerator("t", {
+    description:
+      "Create a new component test file based on current folder name",
+    prompts: [],
+    actions: [
+      {
+        type: "add",
+        path: currentWorkingDirectory + `/${name}.spec.tsx`,
+        templateFile: "plop-templates/component.spec.tsx.hbs",
+        data: { name },
       },
     ],
   });
